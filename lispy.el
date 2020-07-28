75b6dfb426a478ea9a581a5f8c1b4d1642a8e1c9
Sorawee Porncharoenwase <sorawee.pwase@gmail.com>
2020-07-28 02:25:40 -0700

    try to make navigation nicer

--- lispy.el
+++ lispy.el
@@ -5,7 +5,7 @@
            (equal (1+ old-point) new-point))))
 
 ;; from https://github.com/noctuid/lispyville/pull/49/files
-(evil-define-motion my/lispyville-forward-atom-end (count)
+(evil-define-motion my/forward-atom-end (count)
   "Go to the next atom or comment end COUNT times."
   :type inclusive
   (or count (setq count 1))
@@ -21,7 +21,7 @@
           (cl-return))))
     (backward-char)))
 
-(evil-define-motion my/lispyville-forward-sexp (count)
+(evil-define-motion my/forward-sexp (count)
   "This is an evil motion equivalent of `forward-sexp'."
   (let ((original-point (point)))
     (when (nth 3 (syntax-ppss))
@@ -36,7 +36,7 @@
         (error nil)))
     (backward-char)))
 
-(evil-define-motion my/lispyville-backward-sexp (count)
+(evil-define-motion my/backward-sexp (count)
   "This is an evil motion equivalent of `backward-sexp'."
   (let ((original-point (point)))
     (forward-char)
@@ -51,18 +51,32 @@
           (backward-sexp (or count 1))
         (error nil)))))
 
+(evil-define-motion my/down-list (count)
+  "This is an evil motion equivalent of `down-list'."
+  (down-list (or count 1))
+  (my/forward-sexp)
+  (my/backward-sexp))
+
+(evil-define-motion my/backward-up-list (count)
+  "This is an evil motion equivalent of `backward-up-list'."
+  (let ((original-point (point)))
+    (my/backward-sexp)
+    (let ((this-point (point)))
+      (condition-case nil
+          (backward-up-list (or count 1))
+        (error nil))
+      (when (equal this-point (point))
+        (goto-char original-point)))))
+
+(map! :nmv "H" #'my/backward-sexp
+      :nmv "L" #'my/forward-sexp
+      :nmv "E" #'my/forward-atom-end
+      :nmv "U" #'my/backward-up-list
+      :nmv "Y" #'my/down-list)
+
 (after! lispyville
-  (lispy-set-key-theme '(lispy c-digits))
-  (map! (:map lispy-mode-map
-         :nmv "H" #'my/lispyville-backward-sexp
-         :nmv "L" #'my/lispyville-forward-sexp
-         :nmv "E" #'my/lispyville-forward-atom-end
-         :nmv "U" #'lispyville-backward-up-list)
-        (:map lispy-mode-map-lispy
-         "[" nil
-         "]" nil
-         "}" nil
-         ":" nil))
+  (lispy-set-key-theme '())
+
   (lispyville-set-key-theme
    '((operators normal)
      (prettify insert)
