1f0019898526ae1361ae2eb963b5a9bfb2be1e2c
Porncharoenwase <soraweep@b0f1d8726851.ant.amazon.com>
2022-06-13 17:46:11 -0700

    save

--- packages.el
+++ packages.el
@@ -5,7 +5,9 @@
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
 
+
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
+;(package! some-package)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
